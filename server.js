72472c3fd7bec4f0473d04cc1b2a0937a17cdd62
jimmyww7710 <36302780+jimmyww7710@users.noreply.github.com>
2025-01-18 16:19:35 +0800

    feat: Remove the app.listen() code since Vercel handles the serverless function invocation automatically.

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ mongoose.connect(process.env.MONGO_URI, {
 // Routes
 app.use('/api', postRoutes);
 
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// // Start the server
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
