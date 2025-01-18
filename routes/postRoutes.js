02f37b27875cb03d38470cf437f908a6bfc8c385
jimmyww7710 <36302780+jimmyww7710@users.noreply.github.com>
2025-01-18 15:42:59 +0800

    defined script command

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
 
-// Create a new post
-router.post('/posts', postController.createPost);
+// // Create a new post
+// router.post('/posts', postController.createPost);
 
 // Get all posts
 router.get('/posts', postController.getAllPosts);
@@ -11,10 +11,10 @@ router.get('/posts', postController.getAllPosts);
 // Get a single post by ID
 router.get('/posts/:id', postController.getPostById);
 
-// Update a post by ID
-router.put('/posts/:id', postController.updatePost);
+// // Update a post by ID
+// router.put('/posts/:id', postController.updatePost);
 
-// Delete a post by ID
-router.delete('/posts/:id', postController.deletePost);
+// // Delete a post by ID
+// router.delete('/posts/:id', postController.deletePost);
 
 module.exports = router;
